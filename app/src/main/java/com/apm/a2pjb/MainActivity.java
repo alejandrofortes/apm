5814c006efc1355cc92c0ccbb86b421c05288772
Pablo Gómez Area <pablo@MacBook-Pro-de-Pablo.local>
2017-05-24 18:10:34 +0200

    Merge de proyecto (intento 1)

--- app/src/main/java/com/apm/a2pjb/MainActivity.java
+++ app/src/main/java/com/apm/a2pjb/MainActivity.java
@@ -2,28 +2,28 @@ package com.apm.a2pjb;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.content.DialogInterface;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.apm.a2pjb.model.Teacher;
 import com.apm.a2pjb.model.TeacherDB;
+import com.apm.a2pjbAR.UnityPlayerNativeActivity;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
@@ -91,8 +91,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     }
 
     public void openNavigation(View view) {
-        Intent intent = new Intent(this, NavigationActivity.class);
-        intent.putExtra("teachersLoaded", teachersLoaded);
+        Intent intent = new Intent(this, UnityPlayerNativeActivity.class);
         startActivity(intent);
     }
 
